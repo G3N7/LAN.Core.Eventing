1e65efa6bbfaf37c5180a041ac1af42fe156d408
Mitch Ferrer <mr.ferfer@gmail.com>
2014-12-30 22:26:59 -0600

    Working on a better debugging experience: Will now throw a custom exception with the CLR Exception as an Inner, this seems consistent with other libraries and should help the debugging. When the debugger is attached the message returned to the client will contain debugging information. Better wording/formatting for error messages

--- LAN.Core.Eventing.SignalR/SignalREventHub.cs
+++ LAN.Core.Eventing.SignalR/SignalREventHub.cs
@@ -18,7 +18,7 @@ namespace LAN.Core.Eventing.SignalR
 		private readonly ISignalRGroupRegistrar _groupRegistrar;
 		private readonly IGroupJoinService _groupJoinService;
 		private readonly IGroupLeaveService _groupLeaveService;
-		
+
 		public SignalREventHub()
 		{
 			var container = ContainerRegistery.DefaultContainer;
@@ -39,12 +39,17 @@ namespace LAN.Core.Eventing.SignalR
 				foreach (var groupToJoin in groupsToJoin.Result)
 				{
 					this._groupJoinService.JoinToGroup(groupToJoin, this.Context.ConnectionId);
-					Debug.WriteLine("SignalR: {0} has joined group {1}", username, groupToJoin);
+					Debug.WriteLine("SignalR EventHub: {0} has joined group {1}", username, groupToJoin);
 				}
 			}
-			catch (Exception)
+			catch (Exception ex)
 			{
-				SendError("Error Joining Groups");
+				var message = "";
+				if (Debugger.IsAttached)
+				{
+					message = Environment.NewLine + ex.Message;
+				}
+				SendError("Error Joining Groups" + message);
 			}
 
 			return base.OnConnected();
@@ -60,12 +65,18 @@ namespace LAN.Core.Eventing.SignalR
 				foreach (var groupToJoin in groupsToLeave.Result)
 				{
 					this._groupLeaveService.LeaveGroup(groupToJoin, this.Context.ConnectionId);
-					Debug.WriteLine("SignalR: {0} has left group {1}", username, groupToJoin);
+					Debug.WriteLine("SignalR EventHub: {0} has left group {1}", username, groupToJoin);
 				}
 			}
-			catch (Exception)
+			catch (Exception ex)
 			{
-				SendError("Error Leaving Groups");
+				var message = "";
+				if (Debugger.IsAttached)
+				{
+					message = Environment.NewLine + ex.Message;
+				}
+
+				SendError("Error Leaving Groups" + message);
 			}
 
 			return base.OnDisconnected(stopCalled);
@@ -81,10 +92,10 @@ namespace LAN.Core.Eventing.SignalR
 				if (!this._handlerRepository.TryGetHandler(eventName, out handler))
 				{
 					var errorMessage = string.Format(
-						"The event {0} is not a known event, there are only a few things this could be, check that you have Registered the Handler, and that its registered with the event you are emitting, and that all of the handlers dependancies are registered with the DI container.",
+						"The event {0} is not a known event, there are only a few things this could be, check that you have Registered the Handler with your HandlerRepository, that its registered with the event you are emitting {0}, and that all of the handler's dependancies are registered with your DI container.",
 						eventName);
 					SendError(errorMessage);
-					throw new ArgumentException(errorMessage);
+					throw new SignalREventingException(errorMessage, new ArgumentException(errorMessage, "eventName"));
 				}
 
 				if (!handler.IsAuthorized(this.Context.User))
@@ -93,25 +104,28 @@ namespace LAN.Core.Eventing.SignalR
 						"Auth: You are not authorized to use the event {0}.",
 						eventName);
 					SendError(errorMessage);
-					throw new AuthenticationException(errorMessage);
+					throw new SignalREventingException(errorMessage, new AuthenticationException(errorMessage));
 				}
 
 				var eventTask = InvokeHandlerAsync(handler, data);
 				eventTask.ContinueWith(HandleErrorForAsyncHandler, TaskContinuationOptions.OnlyOnFaulted);
 				eventTask.Start();
-				Debug.WriteLine("SignalR: {0} has raised {1} event", this.Context.User.Identity.Name, eventName);
+				Debug.WriteLine("SignalR EventHub: {0} has raised {1} event", this.Context.User.Identity.Name, eventName);
 			}
 			catch (Exception ex)
 			{
-				Debug.WriteLine(ex.ToString());
-#if DEBUG
-				SendError(ex.ToString());
-#endif
+				var message = "";
+				if (Debugger.IsAttached)
+				{
+					message = Environment.NewLine + ex.ToString();
+				}
+				SendError("Unknown Error" + message);
 			}
 		}
 
 		private void SendError(string message)
 		{
+			Debug.WriteLine("SignalR EventHub: Error: \n{0}", message);
 			this._messagingContext.PublishToClient(new EventName(ServerEvents.OnError), new OnErrorResponse(null) { CorrelationId = this.Context.ConnectionId, Message = message });
 		}
 
---
+++ LAN.Core.Eventing.SignalR/SignalREventingException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAN.Core.Eventing.SignalR
+{
+	[Serializable]
+	public class SignalREventingException : Exception
+	{
+		public SignalREventingException() { }
+		public SignalREventingException(string message) : base(message) { }
+		public SignalREventingException(string message, Exception inner) : base(message, inner) { }
+		protected SignalREventingException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context)
+			: base(info, context) { }
+	}
+}
