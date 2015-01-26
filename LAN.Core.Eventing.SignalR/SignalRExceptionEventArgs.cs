beb03de1faaec0cd0481ff20a58646b54439f245
Mitch <mferrer@appriver.com>
2015-01-26 14:40:29 -0600

    adding what connection id caused the issue

--- LAN.Core.Eventing.SignalR/SignalRExceptionEventArgs.cs
+++ LAN.Core.Eventing.SignalR/SignalRExceptionEventArgs.cs
@@ -4,10 +4,14 @@ namespace LAN.Core.Eventing.SignalR
 {
 	public class SignalRExceptionEventArgs : EventArgs
 	{
-		public SignalRExceptionEventArgs(Exception ex)
+		public SignalRExceptionEventArgs(Exception ex, string connectionId)
 		{
 			this.Exception = ex;
+			this.ConnectionId = connectionId;
 		}
+
+		public string ConnectionId { get; private set; }
+
 		public Exception Exception { get; private set; }
 	}
 }
