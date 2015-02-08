30b7260bf98a75fd5f2c18459f7f313423baddf0
Mitch Ferrer <mr.ferfer@gmail.com>
2015-02-08 12:50:03 -0600

    updating signalr

--- LAN.Core.Eventing.SignalR/Properties/AssemblyInfo.cs
+++ LAN.Core.Eventing.SignalR/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LAN.Core.Eventing.SignalR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A SignalR implementation of the LAN.Core.Eventing eventing abstraction for C# to handle events with directionality.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LAN.Core.Eventing.SignalR")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
