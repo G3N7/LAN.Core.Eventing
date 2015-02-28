f1c52a8dc15fd8ac4825e9991a1670f086f50973
jshallard <jshallard@appriver.com>
2015-02-28 17:42:29 +0100

    Added ability for SignalRGroupsService to track Groups by connectionID, and added IGroupLookupService to expose Moved ConnectionLookup Class to outerscope, and renamed to ThreadSafeStringLookup to broaden use.

--- LAN.Core.Eventing.SignalR/SignalRGroupService.cs
+++ LAN.Core.Eventing.SignalR/SignalRGroupService.cs
@@ -1,19 +1,44 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Microsoft.AspNet.SignalR;
 
 namespace LAN.Core.Eventing.SignalR
 {
-	public class SignalRGroupService : IGroupJoinService, IGroupLeaveService
+	public class SignalRGroupService : IGroupJoinService, IGroupLeaveService, IGroupLookupService
 	{
-		public void JoinToGroup(string groupToJoin, string uniqueId)
+		protected static ConcurrentDictionary<string, ThreadSafeStringLookup> ConnectionLookups { get; private set; }
+
+		public void JoinToGroup(string groupToJoin, string connectionId)
 		{
 			var context = GlobalHost.ConnectionManager.GetHubContext<SignalREventHub>();
-			context.Groups.Add(uniqueId, groupToJoin);
+			context.Groups.Add(connectionId, groupToJoin);
+
+			var lookup = ConnectionLookups.GetOrAdd(
+				connectionId,
+				s => new ThreadSafeStringLookup());
+
+			lookup.AddConnectionId(groupToJoin);
+
 		}
 
-		public void LeaveGroup(string groupToLeave, string uniqueId)
+		public void LeaveGroup(string groupToLeave, string connectionId)
 		{
 			var context = GlobalHost.ConnectionManager.GetHubContext<SignalREventHub>();
-			context.Groups.Remove(uniqueId, groupToLeave);
+			context.Groups.Remove(connectionId, groupToLeave);
+
+			ThreadSafeStringLookup lookup;
+			if (ConnectionLookups.TryGetValue(connectionId, out lookup))
+			{
+				lookup.RemoveConnectionId(connectionId);
+			}
+		}
+
+		public IEnumerable<string> GetSignalRGroupsForConnectionId(string connectionId)
+		{
+			ThreadSafeStringLookup lookup;
+			return ConnectionLookups.TryGetValue(connectionId, out lookup) ? lookup.Strings.AsEnumerable() : Enumerable.Empty<string>();
 		}
 	}
 }
---
+++ LAN.Core.Eventing/IGroupLookupService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace LAN.Core.Eventing
+{
+	[ContractClass(typeof (IGroupLookupServiceContract))]
+	public interface IGroupLookupService
+	{
+		IEnumerable<string> GetSignalRGroupsForConnectionId(string connectionId);
+	}
+
+	[ContractClassFor(typeof (IGroupLookupService))]
+	abstract class IGroupLookupServiceContract : IGroupLookupService
+	{
+		public IEnumerable<string> GetSignalRGroupsForConnectionId(string connectionId)
+		{
+			Contract.Requires(connectionId != null);
+			throw new System.NotImplementedException();
+		}
+	}
+}
