4c79558b964f3687682e21f87e9a228c87a8aba8
Mitch Ferrer <mr.ferfer@gmail.com>
2015-03-10 20:59:58 -0500

    adding ConnectionContext to the ISignalRGroupRegistrar

--- LAN.Core.Eventing.SignalR/ISignalRGroupRegistrar.cs
+++ LAN.Core.Eventing.SignalR/ISignalRGroupRegistrar.cs
@@ -6,15 +6,16 @@ namespace LAN.Core.Eventing.SignalR
 	[ContractClass(typeof(SignalRGroupRegistrarContract))]
 	public interface ISignalRGroupRegistrar
 	{
-		Task<string[]> GetGroupsForUser(string usersName);
+		Task<string[]> GetGroupsForUser(string usersName, IConnectionContext connectionContext);
 	}
 
 	[ContractClassFor(typeof(ISignalRGroupRegistrar))]
 	abstract class SignalRGroupRegistrarContract : ISignalRGroupRegistrar
 	{
-		Task<string[]> ISignalRGroupRegistrar.GetGroupsForUser(string usersName)
+		Task<string[]> ISignalRGroupRegistrar.GetGroupsForUser(string usersName, IConnectionContext connectionContext)
 		{
 			Contract.Requires(usersName != null);
+			Contract.Requires(connectionContext != null);
 			Contract.Ensures(Contract.Result<Task<string[]>>() != null);
 			throw new System.NotImplementedException();
 		}
