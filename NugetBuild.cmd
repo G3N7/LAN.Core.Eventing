b2a658f84dafa21c9de8e6909a433a85decb4630
Mitch Ferrer <mr.ferfer@gmail.com>
2015-04-21 09:42:01 -0700

    fixing nuget build script to do release mode, and output symbols

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -1,12 +1,12 @@
 @echo off
 @echo !! Rebuild in Release mode prior to running !!
 echo.
-.nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 echo.
 @echo Finished Building: LAN.Core.Eventing
 echo.
 @echo ---------------------------------------------------
-.nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 echo.
 @echo Finished Building: LAN.Core.Eventing.SignalR
 echo.
