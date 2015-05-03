9392f435920916e4b549c2dec0ea8bdda6541adb
Mitch Ferrer <mr.ferfer@gmail.com>
2015-05-03 17:55:47 -0600

    better build script that will push the package and the symbols out.

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -1,17 +1,28 @@
 @echo off
-@echo !! Rebuild in Release mode prior to running !!
+
+set /p apiKey=Enter API Key:%=%
+.nuget\nuget setApiKey %apiKey%
+
+echo.
 echo.
 .nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 echo.
+echo.
 @echo Finished Building: LAN.Core.Eventing
 echo.
-@echo ---------------------------------------------------
+echo.
 .nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
+echo.
 echo.
 @echo Finished Building: LAN.Core.Eventing.SignalR
 echo.
-@echo ---------------------------------------------------
 echo.
-@echo !! Rebuild in Release mode prior to running !!
+
+set /p eventingVersion=Enter DependencyInjection Package Version:%=%
+.nuget\nuget push LAN.Core.Eventing.%eventingVersion%.nupkg
 echo.
-pause
+echo.
+set /p eventingSignalrVersion=Enter Ninject Package Version:%=%
+.nuget\nuget push LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
+
+echo.
