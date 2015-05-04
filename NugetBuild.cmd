af280b7003c3216bdce59db5611e8f2d79af0e3f
Mitch Ferrer <mr.ferfer@gmail.com>
2015-05-03 18:03:04 -0600

    shorter with better output/behavior

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -3,26 +3,13 @@
 set /p apiKey=Enter API Key:%=%
 .nuget\nuget setApiKey %apiKey%
 
-echo.
-echo.
 .nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
-echo.
-echo.
 @echo Finished Building: LAN.Core.Eventing
-echo.
-echo.
+
 .nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
-echo.
-echo.
 @echo Finished Building: LAN.Core.Eventing.SignalR
-echo.
-echo.
 
-set /p eventingVersion=Enter DependencyInjection Package Version:%=%
+set /p eventingVersion=Enter Eventing Package Version:%=%
 .nuget\nuget push LAN.Core.Eventing.%eventingVersion%.nupkg
-echo.
-echo.
-set /p eventingSignalrVersion=Enter Ninject Package Version:%=%
+set /p eventingSignalrVersion=Enter Eventing Signalr Package Version:%=%
 .nuget\nuget push LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
-
-echo.
