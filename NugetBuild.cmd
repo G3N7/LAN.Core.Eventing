0562c7406ded24f06f7c206cb7c322b2ec88c04b
Mitch Ferrer <mitch@lifeafternormal.net>
2015-07-13 17:44:18 -0600

    making the scripts better

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -1,16 +1,26 @@
-@echo off
+@ECHO off
 
-set /p apiKey=Enter API Key:%=%
+SET /p apiKey=Enter API Key: %=%
 .nuget\nuget setApiKey %apiKey%
 
-.nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
-@echo Finished Building: LAN.Core.Eventing
+SET /p deployEventing=Would you like to deploy the Core Project? (y/n) %=%
+IF (%deployEventing%) EQU (y) (
+	.nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
+	@ECHO Finished Building: LAN.Core.Eventing
 
-set /p eventingVersion=Enter Eventing Package Version:%=%
-.nuget\nuget push LAN.Core.Eventing.%eventingVersion%.nupkg
+	SET /p eventingVersion=Enter Eventing Package Version? %=%
+	@ECHO Publishing LAN.Core.Eventing.%eventingVersion%.nupkg
+	.nuget\nuget push LAN.Core.Eventing.%eventingVersion%.nupkg
+)
 
-.nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
-@echo Finished Building: LAN.Core.Eventing.SignalR
+SET /p deploySignalR=Would you like to deploy the SignalR Project? (y/n) %=%
+IF (%deploySignalR%) EQU (y) (
+	.nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
+	@ECHO Finished Building: LAN.Core.Eventing.SignalR
 
-set /p eventingSignalrVersion=Enter Eventing Signalr Package Version:%=%
-.nuget\nuget push LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
+	SET /p eventingSignalrVersion=Enter Eventing SignalR Package Version? %=%
+	@ECHO Publishing LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
+	.nuget\nuget push LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
+)
+
+@ECHO on
