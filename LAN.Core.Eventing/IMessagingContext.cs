c1ae1bcad1a80f6b04d834c08b19cce2f08dbe7c
Mitch Ferrer <mitch@lifeafternormal.net>
2015-07-13 21:09:07 -0600

    documenting IMessagingContext with XML docs

--- LAN.Core.Eventing/IMessagingContext.cs
+++ LAN.Core.Eventing/IMessagingContext.cs
@@ -6,11 +6,62 @@ namespace LAN.Core.Eventing
 	[ContractClass(typeof(ContractMessagingContext))]
 	public interface IMessagingContext
 	{
+		/// <summary>
+		/// Publishes a response to the client that initiated the request.
+		/// </summary>
+		/// <typeparam name="TResponse">The specific type being used to respond.</typeparam>
+		/// <param name="name">The <see cref="EventName"/> you wish to sent back to the caller</param>
+		/// <param name="response">The body of a response to this request</param>
+		/// <returns>A task that will send the response to the client.</returns>
 		Task PublishToClient<TResponse>(EventName name, TResponse response) where TResponse : ResponseBase;
+
+		/// <summary>
+		/// Publish a response to all connected clients.
+		/// </summary>
+		/// <typeparam name="TResponse">The specific type being used to respond.</typeparam>
+		/// <param name="name">The <see cref="EventName"/> you wish to sent back to the all connected clients.</param>
+		/// <param name="response">The body of a response to this request.</param>
+		/// <returns>A task that will send the response to all connected clients.</returns>
 		Task PublishToAll<TResponse>(EventName name, TResponse response) where TResponse : ResponseBase;
+
+		/// <summary>
+		/// Publish a response to all members of a specific group (with the exception of the caller, please also PublishToClient, this is to prevent double dispatch).
+		/// </summary>
+		/// <typeparam name="TResponse">The specific type being used to respond.</typeparam>
+		/// <param name="groupName">The specific group to send too.</param>
+		/// <param name="name">The <see cref="EventName"/> you wish to sent back to all members of the group.</param>
+		/// <param name="response">The body of a response to this request.</param>
+		/// <returns>A task that will send the response to all members of the specified <see cref="groupName"/>.</returns>
 		Task PublishToGroup<TResponse>(string groupName, EventName name, TResponse response) where TResponse : ResponseBase;
+
+		/// <summary>
+		/// Publish a response to all members of a range of groups (with the exception of the caller, please also PublishToClient, this is to prevent double dispatch).
+		/// </summary>
+		/// <typeparam name="TResponse">The specific type being used to respond.</typeparam>
+		/// <param name="groupNames">the specific set of groups to send too.</param>
+		/// <param name="name">The <see cref="EventName"/> you wish to sent back to all members of the groups.</param>
+		/// <param name="response">The body of a response to this request.</param>
+		/// <returns>A task that will send the response to all members of the specified <see cref="groupNames"/>.</returns>
 		Task PublishToGroups<TResponse>(string[] groupNames, EventName name, TResponse response) where TResponse : ResponseBase;
+
+		/// <summary>
+		/// Push a message to all members of a specific group.
+		/// </summary>
+		/// <typeparam name="TPush">The specific type being used to push to the group.</typeparam>
+		/// <param name="groupName">The specific group to send too.</param>
+		/// <param name="name">The <see cref="EventName"/> you wish to be pushed to all members of the groups</param>
+		/// <param name="pushMessage">The body of the message to push to the group</param>
+		/// <returns>A task that will push the message to all members of the specified <see cref="groupName"/>.</returns>
 		Task PushToGroup<TPush>(string groupName, EventName name, TPush pushMessage) where TPush : PushBase;
+
+		/// <summary>
+		/// Push a message to all members of a specific groups.
+		/// </summary>
+		/// <typeparam name="TPush">The specific type being used to push to the group.</typeparam>
+		/// <param name="groupNames">the specific set of groups to send too.</param>
+		/// <param name="name">The <see cref="EventName"/> you wish to sent back to all members of the groups.</param>
+		/// <param name="pushMessage">The body of the message to push to the groups</param>
+		/// <returns>A task that will push the message to all members of the specified <see cref="groupNames"/>.</returns>
 		Task PushToGroups<TPush>(string[] groupNames, EventName name, TPush pushMessage) where TPush : PushBase;
 	}
 
