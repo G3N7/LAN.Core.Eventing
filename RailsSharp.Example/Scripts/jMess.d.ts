3aef2f8a6bb0421e606d83b361fc96b669bf5942
Mitch Ferrer <mitch@lifeafternormal.com>
2016-02-24 23:45:20 -0700

    updating dependencies and upgrading typescript

--- RailsSharp.Example/Scripts/jMess.d.ts
+++ RailsSharp.Example/Scripts/jMess.d.ts
@@ -7,7 +7,9 @@ declare module jMess {
         private _onRaise;
         constructor(logR: ILogR, onRaise: (event: string, data: Object) => void, timeout?: (delegate: () => void, delay: number) => void);
         getAvailableEvents(): string[];
+        getHooksForEvent(eventName: string): Function[];
         hook(eventToHook: string, delegate: Function): () => void;
+        hookOnce(eventToHook: string, delegate: Function): void;
         raise(eventToRaise: string, data: Object): void;
         register(eventsToRegister: string | string[] | Object): void;
         private _registerEventsObject(eventsObj);
@@ -20,6 +22,7 @@ declare module jMess {
     interface IEventRegistry {
         getAvailableEvents(): string[];
         hook(eventName: string, onRaise: Function): () => void;
+        hookOnce(eventToHook: string, delegate: Function): any;
         raise(eventToRaise: string, data: Object): void;
         register(eventsToRegister: any): void;
     }
--- RailsSharp.Example/Scripts/jMess.js
+++ RailsSharp.Example/Scripts/jMess.js
@@ -12,6 +12,9 @@ var jMess;
             var eventCopy = _.clone(this._events);
             return _.values(eventCopy);
         };
+        EventRegistry.prototype.getHooksForEvent = function (eventName) {
+            return this._registry[eventName];
+        };
         EventRegistry.prototype.hook = function (eventToHook, delegate) {
             if (eventToHook == null)
                 throw 'You must provide an event to hook, have you define the event object yet? "...oh you have to write the code! -Scott Hanselman"';
@@ -19,7 +22,7 @@ var jMess;
                 throw 'You must provide an delegate to run when the event is raised';
             if (!this._eventExists(eventToHook))
                 throw 'The event "' + eventToHook + '" your trying to hook to does not exist, make sure you have registered the events with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) { return '\n' + x; });
-            this._logR.trace('Hooking: ', eventToHook);
+            this._logR.trace('Registering hook: ', eventToHook);
             if (this._registry[eventToHook] == null) {
                 this._registry[eventToHook] = [delegate];
             }
@@ -28,11 +31,35 @@ var jMess;
             }
             var cancelation = (function (r, e, d) {
                 return function () {
+                    logR.trace("Removing hook: ", e);
                     var indexOfDelegate = r[e].indexOf(d);
                     r[e].splice(indexOfDelegate, 1);
                 };
             })(this._registry, eventToHook, delegate);
             return cancelation;
+        };
+        EventRegistry.prototype.hookOnce = function (eventToHook, delegate) {
+            if (eventToHook == null)
+                throw 'You must provide an event to hook, have you define the event object yet? "...oh you have to write the code! -Scott Hanselman"';
+            if (delegate == null)
+                throw 'You must provide an delegate to run when the event is raised';
+            if (!this._eventExists(eventToHook))
+                throw 'The event "' + eventToHook + '" your trying to hook to does not exist, make sure you have registered the events with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) { return '\n' + x; });
+            if (this._registry[eventToHook] == null) {
+                this._registry[eventToHook] = new Array();
+            }
+            var indexOfDelegate = this._registry[eventToHook].length;
+            this._logR.trace('Registering hook ' + eventToHook + "[" + indexOfDelegate + "]");
+            var cancelation = (function (r, e, i) {
+                return function () {
+                    logR.trace("Removing hook " + e + "[" + indexOfDelegate + "]");
+                    r[e].splice(i, 1);
+                };
+            })(this._registry, eventToHook, indexOfDelegate);
+            this._registry[eventToHook].push(function () {
+                cancelation();
+                delegate(arguments);
+            });
         };
         EventRegistry.prototype.raise = function (eventToRaise, data) {
             var _this = this;
