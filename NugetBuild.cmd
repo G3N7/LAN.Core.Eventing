bcbbac6cb6def553fd24aca50b1d0c329cb455a4
Mitch Ferrer <mr.ferfer@gmail.com>
2016-09-20 19:12:19 -0600

    fixing nuget build script

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -1,26 +1,27 @@
 @ECHO off
+Setlocal EnableDelayedExpansion
 
 SET /p apiKey=Enter API Key: %=%
-.nuget\nuget setApiKey %apiKey%
+.nuget\nuget setApiKey !apiKey!
 
 SET /p deployEventing=Would you like to deploy the Core Project? (y/n) %=%
-IF (%deployEventing%) EQU (y) (
+IF (!deployEventing!) EQU (y) (
 	.nuget\nuget pack LAN.Core.Eventing\LAN.Core.Eventing.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 	@ECHO Finished Building: LAN.Core.Eventing
 
 	SET /p eventingVersion=Enter Eventing Package Version? %=%
-	@ECHO Publishing LAN.Core.Eventing.%eventingVersion%.nupkg
-	.nuget\nuget push LAN.Core.Eventing.%eventingVersion%.nupkg
+	@ECHO Publishing LAN.Core.Eventing.!eventingVersion!.nupkg
+	.nuget\nuget push LAN.Core.Eventing.!eventingVersion!.nupkg
 )
 
 SET /p deploySignalR=Would you like to deploy the SignalR Project? (y/n) %=%
-IF (%deploySignalR%) EQU (y) (
+IF (!deploySignalR!) EQU (y) (
 	.nuget\nuget pack LAN.Core.Eventing.SignalR\LAN.Core.Eventing.SignalR.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 	@ECHO Finished Building: LAN.Core.Eventing.SignalR
 
 	SET /p eventingSignalrVersion=Enter Eventing SignalR Package Version? %=%
-	@ECHO Publishing LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
-	.nuget\nuget push LAN.Core.Eventing.SignalR.%eventingSignalrVersion%.nupkg
+	@ECHO Publishing LAN.Core.Eventing.SignalR.!eventingSignalrVersion!.nupkg
+	.nuget\nuget push LAN.Core.Eventing.SignalR.!eventingSignalrVersion!.nupkg
 )
 
 @ECHO on
